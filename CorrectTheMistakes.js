a48835c044a10d5bdf144e9f20246c89c1edaacf
GlebN_K <nizhnik.gleb@gmail.com>
2023-10-10 23:53:06 +0300

    task

--- CorrectTheMistakes.js
+++ CorrectTheMistakes.js
@@ -1,3 +1,7 @@
 function correct(string) {
    return [...string].map(el => el === "5" ? "S" : el === "0" ? "O" : el === "1" ? "I" : el ).join('')
 }
+
+// function correct(string) {
+//     return [...string].map(a => ({'0':'O','5':'S','1':'I'})[a]||a).join('')
+// }
