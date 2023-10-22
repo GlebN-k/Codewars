8b97e52106d9c9c492344a6128066797bd4705cf
GlebN_K <nizhnik.gleb@gmail.com>
2023-10-22 23:19:27 +0300

    task

--- BuildTower.js
+++ BuildTower.js
@@ -1,12 +1,28 @@
 const towerBuilder = nFloors => {
-    let x = Array(nFloors).fill(2*4)
-    console.log(x)
+    let res = []
 
-    // let result = x.map((el, index, arr) => {
-    //
-    // } )
-    let res
-    for(let i= x.length-1; i >= 0; i-- ) {
-        x.map(el => el.repeat(i))
+    for (let i = 1; i <= nFloors; i++) {
+        res.push(nFloors)
     }
+    for (let i = 1; i <= nFloors; i++) {
+        res.fill('*')
+    }
+
+    let stringArr = res.map((el, index) => el.repeat((index + 1) * 2 - 1))
+
+    return stringArr.map((el, index, arr) => el.length < arr[arr.length - 1].length
+        ? " ".repeat(arr[arr.length - 1].length / 2 - index) + el + " ".repeat(arr[arr.length - 1].length / 2 - index)
+        : el)
+
 }
+
+
+// function towerBuilder(nFloors) {
+//     var tower = [];
+//     for (var i = 0; i < nFloors; i++) {
+//         tower.push(" ".repeat(nFloors - i - 1)
+//             + "*".repeat((i * 2)+ 1)
+//             + " ".repeat(nFloors - i - 1));
+//     }
+//     return tower;
+// }
