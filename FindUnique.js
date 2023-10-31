ea12b877fb7b811357cb48a60036c648031120dd
GlebN_K <nizhnik.gleb@gmail.com>
2023-10-31 23:25:15 +0200

    task

--- FindUnique.js
+++ FindUnique.js
@@ -1,10 +1,11 @@
 function findUniq(arr) {
-    let res = []
 
-    arr.map(el => res.includes(el) ? res.push(el) : null
-    //
-    // let obj = {}
-    // arr.forEach(el => obj[el] ? obj[el] + 1 : obj[el]=0 )
-    // // {[el]:}
-    return res
+    let obj = {}
+    arr.forEach(el => obj[el] ? obj[el]+= 1 : obj[el]=1 )
+
+    for(let [key,value] of Object.entries(obj)) {
+        if(value === 1) {
+            return key
+        }
+    }
 }
