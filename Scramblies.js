7211a83fd7beb28f9922cf2d693b92d01f588f54
GlebN_K <nizhnik.gleb@gmail.com>
2023-11-06 23:08:50 +0200

    added comments / another decision for task

--- Scramblies.js
+++ Scramblies.js
@@ -14,3 +14,9 @@ function scramble(str1, str2) {
 
     return true
 }
+
+
+// function scramble(str1, str2) {
+//     let occurences = str1.split("").reduce((arr, cur) => { arr[cur] ? arr[cur]++ : arr[cur] = 1; return arr; }, {});
+//     return str2.split("").every((character) => --occurences[character] >= 0);
+// }
