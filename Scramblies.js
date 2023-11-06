77cc96c743b85431eb4d6ca8afbcbd33b85a13dc
GlebN_K <nizhnik.gleb@gmail.com>
2023-11-06 22:57:45 +0200

    task

--- Scramblies.js
+++ Scramblies.js
@@ -4,12 +4,12 @@ function scramble(str1, str2) {
     str1.split("").forEach(el => obj1[el] ? obj1[el]++ : obj1[el] = 1)
     str2.split("").forEach(el => obj2[el] ? obj2[el]++ : obj2[el] = 1)
 
-    for (let [key, value] of Object.entries(obj2)) {
-        if (!(key in obj2)) {
+    for (let key of Object.keys(obj2)) {
+        if (!(key in obj1)) {
             return false
 
         }
-        if (obj1[key] > obj2[key]) {
+        if (obj2[key] > obj1[key]) {
             return false
         }
     }
