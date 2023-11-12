07d8b9d088c61ce47103338a5f6cd9515647baba
GlebN_K <nizhnik.gleb@gmail.com>
2023-11-12 22:36:10 +0200

    task

--- CreatePhone.js
+++ CreatePhone.js
@@ -1,3 +1,14 @@
 function createPhoneNumber(numbers){
     return numbers.map((el, index) => index === 0 ? `(${el}` : index === 2 ? `${el}) ` : index === 5 ? `${el}-` : el).join('')
 }
+
+// function createPhoneNumber(numbers){
+//     var format = "(xxx) xxx-xxxx";
+//
+//     for(var i = 0; i < numbers.length; i++)
+//     {
+//         format = format.replace('x', numbers[i]);
+//     }
+//
+//     return format;
+// }
